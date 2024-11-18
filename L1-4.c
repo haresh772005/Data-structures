d8c77d74673ae5960a01c03b7d156b4a3b06acac
haresh772005 <128608301+haresh772005@users.noreply.github.com>
2024-11-18 10:15:55 +0530

    Add files via upload

---
+++ L1-4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n1=0,n2=1,n3,i,n;
+    printf("enter the number : ");
+    scanf("%d",&n);
+    printf("\n%d %d",n1,n2);
+    for(i=2;i<=n;i++) //starting from 2 bcozz 0 and 1 are printed 
+    {  
+        n3=n1+n2;
+        printf(" %d ",n3);
+        n1=n2; //   swapping
+        n2=n3;
+    }
+    return 0;
+    
+}
